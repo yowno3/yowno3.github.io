269c181a0a8a5647d8d559c41b36e8ffb2f99539
yowno3 <103923345+yowno3@users.noreply.github.com>
2022-04-18 00:23:45 -0700

    Add files via upload

---
+++ js/title.js
@@ -0,0 +1,36 @@
+﻿if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
+
+function loaded(){
+	
+	setInterval(loop, 300);
+
+}
+
+var x = 0;
+
+var titleText = [ "|",
+					"|",
+					"Y |",
+					"Y o |",
+					"Y o n|",					
+					"Y o n e |",
+					"Y o n e K | ",	
+					"Y o n e K w | ",						
+					"Y o n e K w e | ",
+					"Y o n e K w e k ",	
+					"Y o n e K w e k !",
+					"Y o n e K w e k ! ! ",										];					
+			
+function loop(){
+
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
